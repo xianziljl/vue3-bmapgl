5981da91d8a75cab8fb9bc03c1e330bc63162843
zhuxianyong <zhuxianyong@baidu.com>
2021-11-27 13:56:34 +0800

    调整文件目录

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { default as Map } from './components/Map/Map.vue'
+
+export { default as ZoomControl } from './components/Controls/ZoomControl.vue'
+export { default as NavigationControl3D } from './components/Controls/NavigationControl3D.vue'
+export { default as ScaleControl } from './components/Controls/ScaleControl.vue'
+
+export { default as CustomOverlay } from './components/Overlays/CustomOverlay.vue'
+export { default as Polyline } from './components/Overlays/Polyline.vue'
+export { default as Polygon } from './components/Overlays/Polygon.vue'
+export { default as Circle } from './components/Overlays/Circle.vue'
--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     ],
     build: {
         lib: {
-            entry: path.resolve(__dirname, 'src/components/index.ts'),
+            entry: path.resolve(__dirname, 'src/index.ts'),
             name: 'vue3-bmapgl',
             formats: ['es'],
             fileName: (format) => `index.js`
